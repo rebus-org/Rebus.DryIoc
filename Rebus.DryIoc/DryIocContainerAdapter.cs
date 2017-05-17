5db6797860b17ab669eded8c2251ff46f3737472
Mogens Heller Grabe <mookid8000@gmail.com>
2017-05-17 22:50:59 +0200

    update deps to b09

--- Rebus.DryIoc/DryIocContainerAdapter.cs
+++ Rebus.DryIoc/DryIocContainerAdapter.cs
@@ -27,8 +27,7 @@ namespace Rebus.DryIoc
         /// </summary>
         public DryIocContainerAdapter(IContainer container)
         {
-            if (container == null) throw new ArgumentNullException(nameof(container));
-            _container = container;
+            _container = container ?? throw new ArgumentNullException(nameof(container));
         }
 
         /// <summary>
@@ -56,6 +55,11 @@ namespace Rebus.DryIoc
         /// </summary>
         public void SetBus(IBus bus)
         {
+            if (_container.IsRegistered<IBus>())
+            {
+                throw new InvalidOperationException("This container instance already has an IBus registration. If you want to host multiple Rebus instance in a single process, please do so in separate container instances.");
+            }
+
             _container.Register(Made.Of(() => GetCurrentMessageContext()));
             _container.Register(typeof(ISyncBus), new DelegateFactory(r => r.Resolve<IBus>().Advanced.SyncBus));
 
